d5f8c1d65b915039f287672564defd3c3c47562e
Kimdeadcartney <kimdeadcartney@email.com>
2026-06-26 21:32:12 +0700

    Fix Google OAuth debug

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Exception;
 
 class GoogleController extends Controller
@@ -18,25 +20,36 @@ class GoogleController extends Controller
     public function handleGoogleCallback()
     {
         try {
-            $user = Socialite::driver('google')->user();
-            
-            $finduser = User::where('google_id', $user->id)->first();
-            
-            if($finduser){
-                Auth::login($finduser);
-                return redirect()->intended('/');
-            }else{
-                $newUser = User::updateOrCreate(['email' => $user->email],[
-                    'name' => $user->name,
-                    'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy') // Password dummy
-                ]);
+            $googleUser = Socialite::driver('google')->user();
 
-                Auth::login($newUser);
-                return redirect()->intended('/');
+            $user = User::where('google_id', $googleUser->id)->first();
+
+            if (!$user) {
+
+                $user = User::updateOrCreate(
+                    [
+                        'email' => $googleUser->email,
+                    ],
+                    [
+                        'name' => $googleUser->name,
+                        'google_id' => $googleUser->id,
+                        'password' => Hash::make(Str::random(32)),
+                    ]
+                );
             }
+
+            Auth::login($user, true);
+
+            return redirect('/');
+
         } catch (Exception $e) {
-            return redirect('login')->with('error', 'Gagal login menggunakan Google.');
+
+            dd([
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ]);
+
         }
     }
 }
